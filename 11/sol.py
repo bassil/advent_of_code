d35f727ff8ded64720f1eb9b3ef74142a4ce3930
Bassil <bassil.tabidi@gmail.com>
2020-12-11 17:33:59 -0500

    found potential bugfix, will implement after workout

--- 11/sol.py
+++ 11/sol.py
@@ -90,64 +90,95 @@ def get_first_seats(seat_states, i, j):
 	first_seats = []
 	# 8 directions - N S E W NE NW SE SW
 	# N - i - 1, i - 2, ..., 0
+	bug # for loops are wrong for all 8 directions - should be for k in range(...): if seat_states[i+k][j+k]
 	for _i in range(i-1, -1, -1):
 		if seat_states[_i][j] != ".":
-			first_seats.append(seat_states[_i][j])
+			# first_seats.append(seat_states[_i][j])
+			first_seats.append((seat_states[_i][j], "N"))
 			break
 	# S - i + 1, i + 2, ..., len(seat_states)
 	for _i in range(i+1, len(seat_states)):
 		if seat_states[_i][j] != ".":
-			first_seats.append(seat_states[_i][j])
+			# first_seats.append(seat_states[_i][j])
+			first_seats.append((seat_states[_i][j], "S"))
 			break
 	# E - j + 1, j + 2, ..., len(seat_states[i])
 	for _j in range(j+1, len(seat_states[i])):
 		if seat_states[i][_j] != ".":
-			first_seats.append(seat_states[i][_j])
+			# first_seats.append(seat_states[i][_j])
+			first_seats.append((seat_states[i][_j], "E"))
 			break
 	# W - j - 1, j - 2, ..., 0
 	for _j in range(j-1, -1, -1):
 		if seat_states[i][_j] != ".":
-			first_seats.append(seat_states[i][_j])
+			# first_seats.append(seat_states[i][_j])
+			first_seats.append((seat_states[i][_j], "W"))
 			break
 	# NE (i - 1, j + 1), (i - 2, j + 2), ..., (i - k, j + k)
 	for _i in range(i-1, -1, -1):
+		break_flag = False
 		for _j in range(j+1, len(seat_states[i])):
-			if _i-1 < 0 or _j+1 > len(seat_states[i]):
+			if _i < 0 or _j+1 > len(seat_states[i]):
+				break_flag = True
 				break
 			if seat_states[_i][_j] != ".":
-				first_seats.append(seat_states[_i][_j])
+				# first_seats.append(seat_states[_i][_j])
+				first_seats.append((seat_states[_i][_j], f"NE:{_i}, {_j}"))
+				break_flag = True
 				break
+		if break_flag:
+			break
 	# NW (i - 1, j - 1), (i - 2, j - 2), ..., (i - k, j - k)
 	for _i in range(i-1, -1, -1):
+		break_flag = False
 		for _j in range(j-1, -1, -1):
-			if _i-1 < 0 or _j-1 < 0:
+			if _i < 0 or _j < 0:
 				break
 			if seat_states[_i][_j] != ".":
-				first_seats.append(seat_states[_i][_j])
+				# first_seats.append(seat_states[_i][_j])
+				first_seats.append((seat_states[_i][_j], "NW"))
+				break_flag = True
 				break
+		if break_flag:
+			break
 	# SE (i + 1, j + 1), (i + 2, j + 2), ..., (i + k, j + k)
 	for _i in range(i+1, len(seat_states)):
+		break_flag = False
 		for _j in range(j+1, len(seat_states[i])):
 			if _i+1 > len(seat_states) or _j+1 > len(seat_states[i]):
+				break_flag = True
 				break
 			if seat_states[_i][_j] != ".":
-				first_seats.append(seat_states[_i][_j])
+				# first_seats.append(seat_states[_i][_j])
+				first_seats.append((seat_states[_i][_j], "SE"))
+				break_flag = True
 				break
+		if break_flag:
+			break
 	# SW (i + 1, j - 1), (i + 2, j - 2), ..., (i + k, j - k)
 	for _i in range(i+1, len(seat_states)):
+		break_flag = False
 		for _j in range(j-1, -1, -1):
-			if _i+1 > len(seat_states) or _j-1 > 0:
+			if _i+1 > len(seat_states) or _j < 0:
+				break_flag = True
 				break
 			if seat_states[_i][_j] != ".":
-				first_seats.append(seat_states[_i][_j])
+				# first_seats.append(seat_states[_i][_j])
+				first_seats.append((seat_states[_i][_j], "SW"))
+				break_flag = True
 				break
-
+		if break_flag:
+			break
 	return first_seats
 
 def has_no_occupied_seats_2(seat_states, i, j):
 	"""returns bool indicating whether 8 adjacent seats are empty"""
 	first_seats = get_first_seats(seat_states, i, j)
 	first_seat_count = len(first_seats)
+	print("has_no_occupied_seats")
+	print("i:", i, "j:", j)
+	print(first_seats)
+	print(first_seat_count)
 	empty_first_seat_count = 0
 
 	for first_seat in first_seats:
@@ -195,7 +226,6 @@ def part_2(seat_states):
 		
 		current_seat_states = next_seat_states
 		next_seat_states = get_next_seat_state_2(current_seat_states)
-
 	return sum([_.count('#') for _ in next_seat_states])
 
 def main():
@@ -206,6 +236,17 @@ def main():
 	for i, line in enumerate(seat_states):
 		seat_states[i] = list(line)
 
+	with open("sample_output_part_2.txt") as f:
+		output_states = f.read().split("\n")
+	for i, line in enumerate(output_states):
+		output_states[i] = list(line)
+	
+	# next_seat_states = part_2(seat_states)
+	# for i in range(len(next_seat_states)):
+	# 	for j in range(len(seat_states[0])):
+	# 		if output_states[i][j] != next_seat_states[i][j]:
+	# 			print(i, j)
+	
 	# print("Part 1:", part_1(seat_states))
 	print("Part 2:", part_2(seat_states))
 	
