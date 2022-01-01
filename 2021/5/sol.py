076a9bcbb690077d81db174516ee66caadea0cab
Bassil <bassil.tabidi@gmail.com>
2022-01-01 12:22:01 -0500

    Adds solution to day 5 part 2

--- 2021/5/sol.py
+++ 2021/5/sol.py
@@ -12,18 +12,23 @@ def get_data(file_name):
     return parsed_data
 
 
-def get_straight_lines(data):
+def get_lines(data):
     straight_lines = []
+    diagonal_lines = []
     for vent_line in data:
         [x1, y1], [x2, y2] = vent_line
         if x1 == x2 or y1 == y2:
             straight_lines.append(vent_line)
-    return straight_lines
+        else:
+            # diagonal line conditions
+            if abs(x1 - x2) == abs(y1 - y2):
+                diagonal_lines.append(vent_line)
+    return straight_lines, diagonal_lines
 
 
-def get_vents_from_line(line):
+def get_vents_from_straight_line(straight_line):
     vents = []
-    [_x1, _y1], [_x2, _y2] = line
+    [_x1, _y1], [_x2, _y2] = straight_line
     if _x1 > _x2:
         x1 = _x2
         x2 = _x1
@@ -45,16 +50,43 @@ def get_vents_from_line(line):
     return vents
 
 
-def part_1(data):
+def get_vents_from_diagonal_line(diagonal_line):
+    vents = []
+    [x1, y1], [x2, y2] = diagonal_line
+    if x1 > x2:
+        if y1 > y2:
+            for i in range(x1 - x2 + 1):
+                vents.append((x1 - i, y1 - i))
+        else:
+            for i in range(x1 - x2 + 1):
+                vents.append((x1 - i, y1 + i))
+    else:
+        if y1 > y2:
+            for i in range(x2 - x1 + 1):
+                vents.append((x1 + i, y1 - i))
+        else:
+            for i in range(x2 - x1 + 1):
+                vents.append((x1 + i, y1 + i))
+    return vents
+
+
+def get_count(straight_lines, diagonal_lines=None):
     ocean_floor = {}
-    straight_lines = get_straight_lines(data)
     for straight_line in straight_lines:
-        vents = get_vents_from_line(straight_line)
+        vents = get_vents_from_straight_line(straight_line)
         for vent in vents:
             if vent in ocean_floor:
                 ocean_floor[vent] += 1
             else:
                 ocean_floor[vent] = 1
+    if diagonal_lines is not None and len(diagonal_lines) != 0:
+        for diagonal_line in diagonal_lines:
+            vents = get_vents_from_diagonal_line(diagonal_line)
+            for vent in vents:
+                if vent in ocean_floor:
+                    ocean_floor[vent] += 1
+                else:
+                    ocean_floor[vent] = 1
     count = 0
     for value in ocean_floor.values():
         if value >= 2:
@@ -62,8 +94,19 @@ def part_1(data):
     return count
 
 
+def part_1(data):
+    straight_lines, diagonal_lines = get_lines(data)
+    return get_count(straight_lines)
+
+
+def part_2(data):
+    straight_lines, diagonal_lines = get_lines(data)
+    return get_count(straight_lines, diagonal_lines=diagonal_lines)
+
+
 if __name__ == "__main__":
     test = False
     file_name = "sample.txt" if test else "input.txt"
     data = get_data(file_name)
     print("Part 1:", part_1(data))
+    print("Part 2:", part_2(data))
