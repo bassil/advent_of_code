6518427d16d0cd2a130749a130d3b9f314428b79
bassil <bassil.tabidi@gmail.com>
2022-12-10 14:40:05 -0500

    day 9 refactor

--- 2022/9/main.py
+++ 2022/9/main.py
@@ -1,97 +1,125 @@
 """Day 9: Rope Bridge"""
-from copy import deepcopy
 
-def parse_input(filename):
+class Knot:
+	def __init__(self):
+		self._x = 0
+		self._y = 0
+
+	@property
+	def x(self) -> int:
+		return self._x
+
+	@property
+	def y(self) -> int:
+		return self._y
+
+	def __add__(self, coords: list[int, int]) -> None:
+		"""
+		coords: [int, int]
+		"""
+		self._x = self.x + coords[0]
+		self._y = self.y + coords[1]
+
+	def __repr__(self) -> str:
+		return f"Knot: x: {self.x}, y: {self.y}"
+
+
+class Rope:
+	def __init__(self, num_knots: int = 2):
+		self._num_knots = num_knots
+		self._knots = [Knot() for knot in range(self._num_knots)]
+		self._direction_map = {
+			"R": [1, 0],
+			"L": [-1, 0],
+			"U": [0, 1],
+			"D": [0, -1]
+		}
+		self._tail_move_map = {
+			(2, 0)	: [-1, 0],
+			(-2, 0)	: [1, 0],
+			(0, 2)	: [0, -1],
+			(0, -2)	: [0, 1],
+			(2, 1)	: [-1, -1],
+			(2, -1) : [-1, 1], 
+			(-2, 1) : [1, -1],
+			(-2, -1): [1, 1],
+			(1, 2)  : [-1, -1],
+			(1, -2) : [-1, 1],
+			(-1, 2) : [1, -1],
+			(-1, -2): [1, 1],
+			(2, 2)	: [-1, -1],
+			(2, -2) : [-1, 1],
+			(-2, 2) : [1, -1],
+			(-2, -2): [1, 1]
+		}
+		self._tail_positions = set()
+
+	@property
+	def head(self) -> Knot:
+		return self._knots[0]
+	
+	@head.setter
+	def head(self, coords: list[int, int]) -> None:
+		self._knots[0] = coords
+
+	@property
+	def tail(self) -> Knot:
+		return self._knots[self._num_knots - 1]
+
+	@tail.setter
+	def tail(self, coords: list[int, int]) -> None:
+		self._knots[len(self._num_knots) - 1] = coords
+
+	@property
+	def tail_position(self) -> tuple[int, int]:
+		return (self.tail.x, self.tail.y)
+
+	@property
+	def num_tail_positions(self) -> int:
+		return len(self._tail_positions)
+
+	def move(self, direction: str) -> None:
+		for i in range(self._num_knots):
+			if i == 0:
+				self._knots[i] + self._direction_map[direction]
+			else:
+				dx = self._knots[i].x - self._knots[i - 1].x
+				dy = self._knots[i].y - self._knots[i - 1].y
+				if abs(dx) > 1 or abs(dy) > 1:
+					self._knots[i] + self._tail_move_map[(dx, dy)]
+
+
+		self._tail_positions.add(self.tail_position)
+
+	def execute(self, instruction: int) -> None:
+		direction, steps = instruction
+		for step in range(steps):
+			self.move(direction)
+
+	def __repr__(self) -> str:
+		return f"Rope: (Head: {self.head}, Tail: {self.tail})"
+
+
+def parse_input(filename: str) -> list[list[str, int]]:
 	with open(filename) as fp:
-		data = [line.split() for line in fp.read().split("\n")]
+		data = [[_[0], int(_[1])] for _ in [line.split() for line in fp.read().split("\n")]]
 	return data
 
 
 def part_1(data):
-	head_coords = (0, 0)
-	previous_head_coords = (0, 0)
-	tail_visited = {(0, 0)}
-
-	for direction, num_steps in data:
-		num_steps = int(num_steps)
-		for step in range(num_steps):
-			move = deepcopy(head_coords)
-			if direction == "R":
-				move = (move[0] + 1, move[1])
-			elif direction == "L":
-				move = (move[0] - 1, move[1])
-			elif direction == "U":
-				move = (move[0], move[1] + 1)
-			elif direction == "D":
-				move = (move[0], move[1] - 1)
-			else:
-				raise Exception("Invalid direction")
-
-			# if applying the move makes H more than dist:1 away from T, move T
-			x_distance = abs(move[0] - previous_head_coords[0])
-			y_distance = abs(move[1] - previous_head_coords[1])
-
-			if x_distance > 1 or y_distance > 1:
-				previous_head_coords = deepcopy(head_coords)
-			tail_visited.add(previous_head_coords)
-			head_coords = deepcopy(move)
-
-	return len(tail_visited)
+	rope = Rope()
+	for instruction in data:
+		rope.execute(instruction)
+	return rope.num_tail_positions
 
 def part_2(data):
-	previous_coords = [(0, 0)] * 10
-	current_coords = [(0, 0)] * 10
-	tail_visited = {(0, 0)}
-	for direction, num_steps in data:
-		num_steps = int(num_steps)
-
-		print("------ instruction:", direction, num_steps)
-
-		for step in range(num_steps):
-
-			print("---- step:", step + 1)
-
-			move = deepcopy(previous_coords[0])
-
-			if direction == "R":
-				move = (1, 0)
-			elif direction == "L":
-				move = (- 1, 0)
-			elif direction == "U":
-				move = (0, 1)
-			elif direction == "D":
-				move = (0, - 1)
-			else:
-				raise Exception("Invalid direction")
-
-			for i in range(len(current_coords)):
-				print("i", i)
-				new_coords = (current_coords[i][0] + move[0], current_coords[i][1] + move[1])
-				if i == 0:
-					current_coords[i] = new_coords
-
-				else:
-					x_distance = abs(current_coords[i - 1][0] - current_coords[i][0])
-					y_distance = abs(current_coords[i - 1][1] - current_coords[i][1])
-
-					if max(x_distance, y_distance) > 1:
-						print("--- update ---")
-						current_coords[i] = new_coords
-
-				
-				print("current:", current_coords[i])
-				print("previous:", previous_coords[i])
-				print()			
-			tail_visited.add(current_coords[9])
-			previous_coords = deepcopy(current_coords)
-
-		print()
-		print()
-	return len(tail_visited)
-
+	rope = Rope(num_knots=10)
+	for instruction in data:
+		rope.execute(instruction)
+	return rope.num_tail_positions
 
 if __name__ == '__main__':
-	filename = "sample_input.txt"
+	filename = "input.txt"
 	data = parse_input(filename)
-	# print("part 1:", part_1(data))
+	print("part 1:", part_1(data))
 	print("part 2:", part_2(data))
