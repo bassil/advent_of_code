8a545e1f2d23f6f2f4476fbf9e82d4c9b566b20f
bassil <bassil.tabidi@gmail.com>
2022-12-10 15:01:01 -0500

    day 11 wrapper

---
+++ 2022/11/main.py
@@ -0,0 +1,22 @@
+"""Day 11: """
+
+
+def parse_input(filename):
+    with open(filename) as fp:
+        data = fp.read().split("\n")
+    return data
+
+
+def part_1(data):
+    pass
+
+
+def part_2(data):
+    pass
+
+
+if __name__ == "__main__":
+    filename = "sample_input.txt"
+    data = parse_input(filename)
+    print("part 1:", part_1(data))
+    print("part 2:", part_2(data))
